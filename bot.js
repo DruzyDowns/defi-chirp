9eb6908f87b41318be2419dff5f3ff6a4cdc5dfd
Andrew Downs <druzydowns@gmail.com>
2020-09-15 23:44:19 -0400

    create bot.js, create input.js, link files

---
+++ bot.js
@@ -0,0 +1,3 @@
+const data = require("./input.js");
+
+console.log(data.getData());
---
+++ input.js
@@ -0,0 +1,5 @@
+const getData = () => {
+  return "This is the data for my tweet!";
+};
+
+exports.getData = getData;
